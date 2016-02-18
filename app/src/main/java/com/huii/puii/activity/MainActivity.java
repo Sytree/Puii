0c59fda1586171b20d2ca6f1da8eeb8fd8a61e96
yinlh <sytree39@gmail.com>
2016-02-18 18:04:18 +0800

    增加greendao配置，butterknife配置，dagger2配置

--- app/src/main/java/com/huii/puii/activity/MainActivity.java
+++ app/src/main/java/com/huii/puii/activity/MainActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.huii.puii.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import dagger.Module;
 
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.button_new_plan)
+    TextView button_new_plan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
