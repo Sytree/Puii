4a45058d7c95018fccc9526142d527ec2726b5ce
yinlh <sytree39@gmail.com>
2016-02-19 11:23:23 +0800

    建立booklist表和yearplan表 新建app类

--- MyDaoGenerator/src/main/java/pl/surecase/eu/MyDaoGenerator.java
+++ MyDaoGenerator/src/main/java/pl/surecase/eu/MyDaoGenerator.java
@@ -7,12 +7,37 @@ import de.greenrobot.daogenerator.Schema;
 public class MyDaoGenerator {
 
     public static void main(String args[]) throws Exception {
-        Schema schema = new Schema(3, "greendao");
-        Entity box = schema.addEntity("Box");
-        box.addIdProperty();
-        box.addStringProperty("name");
-        box.addIntProperty("slots");
-        box.addStringProperty("description");
+        // 1: 数据库版本号
+        // com.xxx.bean:自动生成的Bean对象会放到/java-gen/com/xxx/bean中
+        Schema schema = new Schema(1, "com.puii.bean");
+
+        // DaoMaster.java、DaoSession.java、BeanDao.java会放到/java-gen/com/xxx/dao中
+        schema.setDefaultJavaPackageDao("com.puii.dao");
+
+        //初始化bean
+        initBookListBean(schema);
+        initYearPlanBean(schema);
+
         new DaoGenerator().generateAll(schema, args[0]);
+    }
+
+    private static void initYearPlanBean(Schema schema) {
+        Entity bookListBean = schema.addEntity("YearPlanBean");//表名
+        bookListBean.setTableName("yearplan");//对表命名
+        bookListBean.addStringProperty("id").primaryKey().index();//id键
+        bookListBean.addStringProperty("content");
+        bookListBean.addStringProperty("add_date");
+        bookListBean.addStringProperty("complete_date");
+        bookListBean.addStringProperty("state");
+    }
+
+    private static void initBookListBean(Schema schema) {
+        Entity bookListBean = schema.addEntity("BookListBean");//表名
+        bookListBean.setTableName("booklist");//对表命名
+        bookListBean.addStringProperty("id").primaryKey().index();//id键
+        bookListBean.addStringProperty("name");
+        bookListBean.addStringProperty("add_date");
+        bookListBean.addStringProperty("complete_date");
+        bookListBean.addStringProperty("state");
     }
 }
---
+++ app/src/main/java/com/huii/puii/app/PuiiApp.java
@@ -0,0 +1,14 @@
+package com.huii.puii.app;
+
+import android.app.Application;
+
+/**
+ * Created by yinlh on 2016/2/19.
+ */
+public class PuiiApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
