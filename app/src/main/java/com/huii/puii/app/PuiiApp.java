b67beb088f9f367ef1eb28411dc0d8472885a372
yinlh <sytree39@gmail.com>
2016-02-19 11:39:18 +0800

    app初始化数据库操作

--- app/src/main/java/com/huii/puii/app/PuiiApp.java
+++ app/src/main/java/com/huii/puii/app/PuiiApp.java
@@ -1,14 +1,36 @@
 package com.huii.puii.app;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.huii.puii.business.PuiiDataBaseHelper;
+import com.puii.dao.DaoMaster;
+import com.puii.dao.DaoSession;
 
 /**
  * Created by yinlh on 2016/2/19.
  */
 public class PuiiApp extends Application {
+
+    public DaoSession daoSession;
     @Override
     public void onCreate() {
         super.onCreate();
+        setUpDatabase();
+    }
+    //初始化数据库
+    private void setUpDatabase() {
+//        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "example-db", null);
+//        SQLiteDatabase db = helper.getWritableDatabase();
+//        DaoMaster daoMaster = new DaoMaster(db);
+//        daoSession = daoMaster.newSession();
+        PuiiDataBaseHelper helper = new PuiiDataBaseHelper(getApplicationContext(),"puii-db",null);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        daoSession  = daoMaster.newSession();
+    }
 
+    public DaoSession getDaoSession() {
+        return daoSession;
     }
 }
---
+++ app/src/main/java/com/huii/puii/business/PuiiDataBaseHelper.java
@@ -0,0 +1,31 @@
+package com.huii.puii.business;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.puii.dao.DaoMaster;
+
+/**
+ * Created by yinlh on 2016/2/19.
+ */
+public class PuiiDataBaseHelper extends DaoMaster.OpenHelper {
+    public PuiiDataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+        super(context, name, factory);
+    }
+    //避免更新数据库时将旧数据删除
+    // TODO: 2016/2/19
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        switch (oldVersion){
+            case 1:
+                //创建新表，注意createTable()是静态方法
+                // SchoolDao.createTable(db, true);
+
+                // 加入新字段
+                // db.execSQL("ALTER TABLE 'moments' ADD 'audio_path' TEXT;");
+
+                // TODO
+                break;
+        }
+    }
+}
